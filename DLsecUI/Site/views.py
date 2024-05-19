9329ea035765b693b60a49b42d7bbbab8c496e76
zts <zt-785@sjtu.edu.cn>
2024-05-19 22:00:42 +0800

    ui upload function and setting function补充，尚未完全跑通

--- DLsecUI/Site/views.py
+++ DLsecUI/Site/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render,HttpResponse
-from django.core.files.storage import FileSystemStorage as fs
+from django.http import JsonResponse
+import torch
+from django.core.files.storage import FileSystemStorage 
 from django.http import HttpResponseRedirect
 from django.contrib import messages
-
+from multiprocessing import Process
+import sys
+sys.path.append('../')
+from EvaluationPlatformNEW import ModelEvaluation
+from EvaluationConfig import evaluation_params
 # Create your views here.
 def test(request):
     return HttpResponse("Hello, world.")
@@ -16,15 +22,32 @@ def cv(request):
 def upload(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['model']
-        fs.FileSystemStorage()
+        evaluation_params['model'] = uploaded_file
+        print("change model")
+        fs = FileSystemStorage()
         name=fs.save(uploaded_file.name, uploaded_file)
         file_url=fs.url(name)
+        print(torch.load("."+file_url))
+        
+        return JsonResponse({'result':"success"})
     
 def setting (request):
     if request.method == 'POST':
        status={
            'state':'正在运行',
        }
-            
+    '''
+    获取表单中的数据
+    '''
+    dataset = request.POST.get('dataset')
+    adver = request.POST.get('adver')
+    back = request.POST.get('back')
+    poison = request.POST.get('poison')
+    args = request.POST.get('args')
+    backdoor_method = request.POST.get('backdoor_method')
+    print(dataset,adver,back,poison,args,backdoor_method)
+    print(type(evaluation_params['model']))
+    # 对evaluation_params进行对于修改，一些比较基础的参数无需修改
+    Process(target=ModelEvaluation,args=[evaluation_params]).start()
     return render(request,"cv.html",status)
 
